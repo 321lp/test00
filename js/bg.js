35f874c671f378a73b63f4a1488f7305d196d481
321lp <136479382+321lp@users.noreply.github.com>
2023-06-14 18:16:06 +0800

    1111
    123

---
+++ js/bg.js
@@ -0,0 +1,9 @@
+// w
+//   var button = document.querySelectorAll(".test a");
+//   var bgm = document.querySelectorAll("audio");
+//   console.log(bgm);
+//   console.log(button);
+//   button[0].onclick = function () {
+//     bgm[0].play();
+//   };
+// };
